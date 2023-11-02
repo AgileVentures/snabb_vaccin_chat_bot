b97b4dc9315a4e5e536603d44082447d82695c5e
tochman <thomas@craftacademy.se>
2023-11-02 10:20:22 +0100

    adds UI for chatbot

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import send from "../assets/send.svg";
 
 const Chatbot = () => {
   return (
-    <div>Chatbot</div>
-  )
-}
+    <main>
+      <section className="chatbot-container">
+        <div className="chatbot-header">
+          <p className="sub-heading">Knowledge Bank</p>
+        </div>
+        <div
+          className="chatbot-conversation-container"
+          id="chatbot-conversation-container"
+        ></div>
+        <form id="form" className="chatbot-input-container">
+          <input name="user-input" type="text" id="user-input" required />
+          <button id="submit-btn" className="submit-btn">
+            <img
+              src={send}
+              className="send-btn-icon"
+              alt="Send Icon"
+            />
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
 
-export default Chatbot
+export default Chatbot;
