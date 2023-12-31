23abd47909506850fa66ba566c2f3b4a2a7953c9
tochman <thomas@craftacademy.se>
2023-12-31 12:31:11 +0100

    changes embed structure

--- public/embed.js
+++ public/embed.js
@@ -1,10 +1,32 @@
-(function () {
-  var widgetContainer = document.createElement("div");
-  widgetContainer.id = "vaccin_bot"; // The ID should match the one in your React app
-  document.body.appendChild(widgetContainer);
+// embed.js
+window.mountChatbot = function(selector) {
+  // Wait for the DOM to be fully loaded
+  document.addEventListener('DOMContentLoaded', function () {
+    // Find the container using the selector provided
+    var container = document.querySelector(selector);
+    
+    if (container) {
+      // Clear existing content of the container
+      container.innerHTML = '';
+      
+      // Create the div element for the chatbot
+      var widgetContainer = document.createElement('div');
+      widgetContainer.id = 'vaccin_bot'; // The ID should match the one in your React app
+      
+      // Append the div to the specified container
+      container.appendChild(widgetContainer);
+      
+      // Load the chatbot script using bootstrap.js
+      var script = document.createElement('script');
+      script.src = 'https://chatbot.snabbvaccin.se/bootstrap.js'; // The URL to your bootstrap script
+      script.async = true;
+      document.body.appendChild(script);
+    } else {
+      console.error('The specified container was not found in the DOM.');
+    }
+  });
+};
 
-  var script = document.createElement("script");
-  script.src = "https://chatbot.snabbvaccin.se/bootstrap.js"; // Adjust the URL as needed
-  script.async = true;
-  document.body.appendChild(script);
-})();
+// Example usage:
+// mountChatbot('.book-cta');
+
