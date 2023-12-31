d952c3144cb3f573872bd25dd6748891b3b4fe9e
tochman <thomas@craftacademy.se>
2023-12-31 12:58:30 +0100

    changes embed structure - 3

--- public/bootstrap.js
+++ public/bootstrap.js
@@ -1,9 +1,22 @@
+// bootstrap.js
 fetch("https://chatbot.snabbvaccin.se/manifest.json")
   .then((response) => response.json())
   .then((manifest) => {
+    // Extract the CSS file path from the manifest
+    const cssPath = manifest["index.css"].file;
+
+    // Create a link tag for the CSS and append it to the head
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = `https://chatbot.snabbvaccin.se/${cssPath}`;
+    document.head.appendChild(link);
+
+    // Extract the JS file path from the manifest
     const mainJs = manifest["index.html"].file;
-    const script = document.createElement("script");
-    script.type = 'module'; // This is crucial for ES6 modules
+
+    // Create a script tag for the JS and append it to the body
+    const script = document.createElement('script');
+    script.type = 'module'; // Use module if your script uses ES6 imports/exports
     script.src = `https://chatbot.snabbvaccin.se/${mainJs}`;
     document.body.appendChild(script);
   })
